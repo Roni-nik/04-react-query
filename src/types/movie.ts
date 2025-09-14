90bd2a8d0a2f00bf493be5f41b19712b046288d4
Veronika Kulynych <kulynychron@gmail.com>
2025-09-14 16:24:48 +0300

    first commit

---
+++ src/types/movie.ts
@@ -0,0 +1,12 @@
+// eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NTNlYzI4N2M2NDg2NWYwOGY2ZDMxOGYwYTg3MGJlMyIsIm5iZiI6MTc1NzQ5NDgxNC41NjYwMDAyLCJzdWIiOiI2OGMxM2UxZTUxMDdmZjgzYjUxYmQ3M2IiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.D_Miq_vSSi8QNLhV2X5070z0TGOTE36d0L8XJHJ5G5c
+
+
+export interface Movie {
+    id: number;
+    poster_path: string;
+    backdrop_path: string;
+    title: string;
+    overview: string;
+    release_date: string;
+    vote_average: number;
+}
